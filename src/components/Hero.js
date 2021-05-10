faf0eeba3e31dbc0b48681f2ac12004ffb445ca2
Hessvacio Hassan <hhassan@oswego.edu>
2021-05-10 02:07:41 -0400

    adds gif

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
           <video src="https://anima-uploads.s3.amazonaws.com/projects/60523ff4ec5e2ec86a4add42/files/mibkg-highlight-reel.mp4" loop autoPlay="autoplay" playsInline muted />
         </div>
         <div className="mobile_hero">
-          <img className="hero-image-mobile-C61RwL" src="img/mobile-game-showcase-hero-image-mobile--A17A3CFD-1001-41F0-B69A-8440E534AC7B.jpg" />
+          <img className="hero-image-mobile-C61RwL" src="https://media.giphy.com/media/JoIiLnxitZAcWkAJBQ/giphy-downsized-large.gif" />
           <div className="overlay-C61RwL"></div>
         </div>
         
