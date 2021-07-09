01aac5af760d689543d5dcd8b865dd78488d34ef
hhassan1230 <hhassan@oswego.edu>
2021-07-08 21:50:03 -0400

    adds mobile hero image

--- src/components/header/Hero.js
+++ src/components/header/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
                         <video alt="" src="https://anima-uploads.s3.amazonaws.com/projects/60523ff4ec5e2ec86a4add42/files/mibkg-highlight-reel.mp4" loop autoplay="autoplay" playsinline muted></video>
                         </div>
                         <div className="mobile_hero">
-                        <img className="hero-image-mobile-C61RwL" alt="" src="assets/img/mobile-game-showcase-hero-image-mobile--A17A3CFD-1001-41F0-B69A-8440E534AC7B.png"/>
+                        <img className="hero-image-mobile-C61RwL" alt="" src="https://media.giphy.com/media/6Uuw2GuTPtlWqEBTSg/giphy.gif"/>
                         <div className="overlay-C61RwL"></div>
                         </div>
                         
